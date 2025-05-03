a60b1856cc0bbef7573a1f6fa53f4adf0a8584ba
Rick <rick-ame@outlook.com>
2025-05-03 18:16:05 +0800

    set project

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+    ],
+    files: ['**/*.{ts,tsx,js}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.app.json',
+        },
+        alias: {
+          map: [['', './public']],
+        },
+      },
+    },
+  },
+)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return <h1 className="text-3xl">Rick AME</h1>
+}
+
+export default App
