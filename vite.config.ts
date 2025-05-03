c527526d2c913888c711d7fe5b2b9226128594bd
Rick <rick-ame@outlook.com>
2025-05-03 20:42:16 +0800

    chore: github pages use sepcific command

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
