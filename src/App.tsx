39aaa61e628e4adb925b9c977e39eaf712bdc285
Rick <rick-ame@outlook.com>
2025-05-04 08:54:14 +0800

    cleanup for 3d portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import { FC } from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
-
-import { Home } from './pages/home'
 
 const App: FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return null
 }
 
 export default App
